10d60c84b6a9246114f1d24378c667fe8938d490
Shimmyshanga <luca.junghans@gmail.com>
2017-07-01 18:35:31 -0700

    deployhotfixing

--- Server/Config/mongoose.js
+++ Server/Config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
         fs = require('fs'),
         imgPath = './../../client/static/img';
         path = require('path'),
-        models_path = path.join(__dirname,"../models"),
+        models_path = path.join(__dirname,"../Models"),
         reg = new RegExp( ".js$", "i" ),
         dbURI = 'mongodb://localhost/DnDMassData';
 
